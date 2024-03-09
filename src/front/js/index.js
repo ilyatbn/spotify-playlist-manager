b645bf97ac9e35b117755cdfba4c920af962b4a6
ilyatovbin <ilya89@outlook.com>
2024-03-09 20:15:55 +0000

    small main menu text

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,3 +1,6 @@
+// WOW! You can see all my code and bypass my great firewall of menus. What a great hacker you are!
+// This whole frontend implementation is just for fun. don't ever write garbage like this yourself.
+//
 async function fetchUserData() {
     // do this once a session, because I don't really wanna overload requests on the BE for playlists.
     if (sessionStorage.hasOwnProperty("managed_spl")) {
@@ -17,7 +20,18 @@ async function fetchUserData() {
   }
 
 async function loggedOutContent() {
-      $('#body').append('<h1 id="disclaimer" class="text-white font-bold pl-5 pt-10">Welcome to SPLAM. Logging in grants us access to your account. We will store access credentials that will allow us to create and update your playlists in the background.</h1>');
+      $('#body').append(`
+        <h1 id="disclaimer" class="text-white font-bold text-xl pl-5 pt-10">Welcome to SPLAM!</h1>
+        <h2 id="disclaimer" class="text-white pl-5 pt-1">SPLAM is a Spotify playlist manager which extends what Spotify can do. </h1>
+        <h2 id="disclaimer" class="text-white pl-5 pt-1">This service is currently in early stages of development.</h1>
+        <h2 id="disclaimer" class="text-white text-xl font-bold pl-5 pt-10">Features</h1>
+        <h2 id="disclaimer" class="text-white pl-5">* [WIP] Genre based playlists. Select one or more source playlists, and create new playlists based on genres. Updated automatically every day.</h1>
+        <h2 id="disclaimer" class="text-white pl-5">* [WIP] Playlist backup and restore. Weekly snapshots.</h1>
+
+        <h2 id="disclaimer" class="text-white text-xl font-bold pl-5 pt-10">Disclaimer</h1>
+        <h2 id="disclaimer" class="text-white pl-5">Logging in grants us access to your Spotify account. We will store access credentials that will allow us to manage playlists your account even if you're not logged in.</h1>
+        <h2 id="disclaimer" class="text-white pl-5">We store metadata about the playlists that you select to manage, as well as some basic details about your account. This data will not be shared to anyone.</h1>
+        `);
       $('#topbarRight').append('<button id="loginButton" class="bg-[#1ed760] hover:bg-[#1ed910] text-black font-bold py-2 px-4 rounded-full">Login with Spotify</button>');
 }
 
@@ -63,7 +77,7 @@ async function menuRouter(refresh=false) {
         $("#create_view").remove()
     }
     if (location.hash === "#view") {
-      console.log("You're view");
+      console.log("You're a teapot");
     }
     if (location.hash === "#create") {
         buildSpotifyPlaylists()
@@ -84,7 +98,6 @@ async function menuRouter(refresh=false) {
             console.log($(this).attr('spl-selected'))
         })
     }
-
 }
 
 function setMenuEvents(){
