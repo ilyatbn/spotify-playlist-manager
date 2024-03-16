f0e5379ab1ec1ef0720c32f2708204ab742b7dbf
ilyatovbin <ilya89@outlook.com>
2024-03-16 17:25:36 +0000

    basic playlist meta v0 WIP + liked songs in list

--- src/core/spotify/playlists.py
+++ src/core/spotify/playlists.py
@@ -1,6 +1,13 @@
+import copy
+
 import requests
 
-from core.spotify.const import SPOTIFY_PLAYLIST_ENDPOINT
+from core.spotify.const import (
+    SPOTIFY_PLAYLIST_ENDPOINT,
+    SPOTIFY_PLAYLISTS_ENDPOINT,
+    SPOTIFY_SAVED_TRACKS_ENDPOINT,
+    SPOTIFY_TRACKS_ENDPOINT,
+)
 from core.spotify.schemas import SpotifyPlaylist
 from exceptions import SpotifyPlaylistRequestException
 
@@ -24,7 +31,17 @@ class UserPlaylistHandler:
 
     def _parse_playlists(self, user_playlists: list) -> list:
         parsed_playlists = []
-        # I'm lazy so this is done like this because I wanna display Liked Songs and Release Radar fist.
+        # liked songs / saved items, isn't really a playlist like others, so we'll add it manually
+        parsed_playlists.append(
+            SpotifyPlaylist(
+                username=self.user.username,
+                playlist_id=f"me",
+                name="Liked Songs",
+                snapshot_id="0",
+                image_url="https://misc.scdn.co/liked-songs/liked-songs-640.png",
+            )
+        )
+        # I'm lazy so this is done like this because I wanna display Liked Songs and Release Radar first.
         for playlist in user_playlists:
             if playlist.get("name").lower() in ALLOWED_NON_OWNER_PLAYLISTS:
                 playlist_item = SpotifyPlaylist(
@@ -53,16 +70,20 @@ class UserPlaylistHandler:
                 parsed_playlists.append(playlist_item)
         return parsed_playlists
 
-    def _get_playlist_chunk(self, next: str = None, start_pos: int = 0) -> tuple:
-        if not next:
+    def _get_chunk(
+        self, url, next_url: str = None, start_pos: int = 0, params: dict = {}
+    ) -> tuple:
+        if not next_url:
+            final_params = copy.deepcopy(params)
+            final_params.update({"offset": start_pos, "limit": 50})
             chunk = requests.get(
-                url=SPOTIFY_PLAYLIST_ENDPOINT.format(user_id=self.user.username),
-                params={"offset": start_pos, "limit": 50},
+                url=url,
+                params=final_params,
                 headers=self.base_headers,
             )
         else:
             chunk = requests.get(
-                url=next,
+                url=next_url,
                 headers=self.base_headers,
             )
 
@@ -72,14 +93,65 @@ class UserPlaylistHandler:
         chunk_dict = chunk.json()
         return chunk_dict["items"], chunk_dict["next"]
 
-    def get_user_playlists(self, start_pos: int = None) -> list:
+    def get_items(self, url, start_pos: int = None, params={}) -> list:
         self.user.auth.refresh_access_token()
-        user_playlists = []
+        api_response_items = []
 
-        chunk, next_playlist = self._get_playlist_chunk(start_pos=start_pos)
-        user_playlists.extend(chunk)
+        chunk, next_playlist = self._get_chunk(
+            url=url,
+            start_pos=start_pos,
+            params=params,
+        )
+        api_response_items.extend(chunk)
         while next_playlist:
-            chunk, next_playlist = self._get_playlist_chunk(next_playlist)
-            user_playlists.extend(chunk)
-        # get liked songs
-        return self._parse_playlists(user_playlists)
+            chunk, next_playlist = self._get_chunk(
+                url=url,
+                next=next_playlist,
+            )
+            api_response_items.extend(chunk)
+        return api_response_items
+
+    def _parse_tracks(self, tracklist: list, start_date=None) -> list:
+        parsed_tracks = []
+        # max_added_at = None
+        for track in tracklist:
+            if (
+                track_added_at := item.get("added_at", None)
+            ) and track_added_at > start_date:
+                item = track
+                parsed_tracks.append(item)
+        return parsed_tracks
+
+    def get_user_playlists(self, start_pos: int = None) -> list:
+        playlist_endpoint = SPOTIFY_PLAYLISTS_ENDPOINT.format(
+            user_id=self.user.username
+        )
+        playlists = self.get_items(url=playlist_endpoint, start_pos=start_pos)
+        return self._parse_playlists(playlists)
+
+    def get_playlist_tracks(
+        self, playlist_id, start_pos: int = 0, start_date=None
+    ) -> list:
+        tracks_endpoint = SPOTIFY_TRACKS_ENDPOINT.format(playlist_id=playlist_id)
+        # meta = SPOTIFY_PLAYLIST_META_ENDPOINT.format(playlist_id=playlist_id)
+        params = {"fields": "items(added_at,track(id,name,artists))"}
+        tracks = self.get_items(url=tracks_endpoint, start_pos=start_pos, params=params)
+        return self._parse_tracks(tracks)
+
+    def get_liked_tracks(self, start_pos: int = 0) -> list:
+        tracks = self.get_items(url=SPOTIFY_SAVED_TRACKS_ENDPOINT, start_pos=start_pos)
+        return self._parse_tracks(tracks)
+
+    def process_tracks(self):
+        pass
+        # sync.
+        # playlist_meta_endpoint = SPOTIFY_PLAYLIST_ENDPOINT
+        # snapshot_id = playlist_meta_endpoint
+        # playlist = get_playlist_from_db.
+        # spotify_playlist = get_playlist_from_db.
+        # if not spotify_playlist.get("snapshot_id") == playlist.snapshot_id:
+        # start_pos = playlist.start_pos-100
+        #
+        # tracks = get_playlist_tracks(start_pos=start_pos, after=playlist.last_added_at),
+        # save last trackss 'added_at'
+        # save total.
